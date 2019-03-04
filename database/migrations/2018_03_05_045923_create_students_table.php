27db47b8d0297b665ff5f9cd2732abe2ee726250
j_polak <jan.polak@onmyway.cz>
2019-03-04 08:36:47 +0100

    corrections

--- database/migrations/2018_03_05_045923_create_students_table.php
+++ database/migrations/2018_03_05_045923_create_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHeroesTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
